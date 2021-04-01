e839703282ca7d55ae035bba75ea14de257af296
Somen <Somensahoo@gmail.com>
2021-04-01 20:47:01 +0530

    image uploading added

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const methodOverride = require('method-override');
+
 const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const port = 3000;
 const mongoose = require("mongoose");
-const Post = require("./models/post");
-const {MONGOURI}=require('./keys');
+const { MONGOURI } = require('./keys');
+const postroutes = require("./routers/postrouter");
 
 
 mongoose.connect(MONGOURI, {
@@ -31,43 +32,14 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+
 app.get('/', (req, res) => {
   res.render("home");
 });
 
-app.get("/posts", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("posts/show", {posts});
-});
+app.use("/posts", postroutes);
 
-app.post("/posts", async (req, res) => {
-  const post = new Post(req.body.post);
-  await post.save();
-  // console.log(post);
-  res.redirect(`/posts/${post.id}`);
-});
-
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("posts/showpost", { post });
-});
-
-app.get("/posts/:id/edit", async (req, res) =>{
-  const post = await Post.findById(req.params.id);
-  res.render("posts/edit", { post });
-});
-
-app.put("/posts/:id", async (req, res) =>{
-  const post = await Post.findByIdAndUpdate(req.params.id, {...req.body.post});
-  await post.save();
-  // console.log(post);
-  res.redirect(`/posts/${post.id}`);
-});
-app.delete("/posts/:id", async(req, res) =>{
-  await Post.findByIdAndDelete(req.params.id);
-  // console.log(req.params.id);
-  res.redirect("/posts");
-})
 app.get("/contacts", (req, res) => {
   res.render("posts/contacts");
 })
